a88628fa3b3f226740eafbc04bf558d761e2207a
joseC <ignacio3437@gmail.com>
2017-01-31 15:19:03 +0800

    Plotting and thin all work

--- RADworkflow.py
+++ RADworkflow.py
@@ -10,11 +10,20 @@ rprint = ro.globalenv.get("print")
 rplot=ro.r('plot')
 
 
-def vcftoPCA(pwd,basename,thin):
+def thinner(pwd,basename):
     os.chdir(pwd)
-    # print commands.getoutput("vcftools --vcf %s.vcf --out %s_thin --thin 500000 --plink"%(basename,basename))
-    print commands.getoutput("awk '$1=1' %s_thin.map > %s_t.map"%(basename,basename))
-    print commands.getoutput("mv %s_thin.ped %s_t.ped"%(basename,basename))
+    thin=basename+'_t'
+    print commands.getoutput("vcftools --vcf %s.vcf --out %s --thin 500000 --recode"%(basename,thin))
+    print commands.getoutput("mv %s.recode.vcf %s.vcf"%(thin,thin))
+    return thin
+
+
+def vcftoPCA(pwd,basename):
+    os.chdir(pwd)
+    baseout=basename+'_o'
+    print commands.getoutput("vcftools --vcf %s.vcf --out %s --plink"%(basename,baseout))
+    print commands.getoutput("awk '$1=1' %s.map > %s_temp.map"%(baseout,baseout))
+    print commands.getoutput("mv %s_temp.map %s.map"%(baseout,baseout))
     with open(pwd+'pca.par','w') as pca:
         pca.write("""
     genotypename:    %s.ped
@@ -28,24 +37,25 @@ def vcftoPCA(pwd,basename,thin):
     grmoutname:      grmjunk
     numthreads:   8
     lsqproject:	YES
-        """%(thin,thin,thin,thin,thin))
-    pcafile='%s%s.evec'%(pwd,thin)
-    # pcaout=commands.getoutput("smartpca -p pca.par")
-    # with open(pwd+'pcaout.txt','w') as pcaoutfile:
-    #     pcaoutfile.write(pcaout)
+    outliersigmathresh: 7
+        """%(baseout,baseout,baseout,baseout,baseout))
+    pcafile='%s%s.evec'%(pwd,baseout)
+    pcaout=commands.getoutput("smartpca -p pca.par")
+    with open(pwd+'pcaout.txt','a') as pcaoutfile:
+        pcaoutfile.write(pcaout)
     return pcafile
 
 
 def RplotPCA(pcafile,pwd,basename):
     #Merges PCA output and Datafile in R then prints XY plots as PNG in pwd.
-    ro.r('evec <- read.table(file="%s%s.evec")'%(pwd,basename))
+    #Datafile must be tsv with headers: Samples, LAT, LON, COI[_cor]
+    ro.r('evec <- read.table(file="%s%s_o.evec")'%(pwd,basename))
     ro.r('dat <- read.table(file="%sdata.txt",header=TRUE)'%(pwd))
     ro.r('m2 <- merge(dat,evec,by.x="Sample",by.y="V1")')
     m2=ro.r['m2']
-    print m2.colnames
-    ##
+    ####
     #Change color scheme by changing COI_cor
-    ##
+    ####
     grdevices.png(file="%s/%s_MAP_COI.png"%(pwd,basename), width=1000, height=1000)
     rplot(m2.rx2("LON"),m2.rx2("LAT"),col=m2.rx2("COI_cor"),main="%s_MAP_COI"%(basename),ylab="Lattitude",xlab="Longitude",pch=20,cex=1.5)
     grdevices.png(file="%s/%s_PCA_COI.png"%(pwd,basename), width=1000, height=1000)
@@ -54,15 +64,19 @@ def RplotPCA(pcafile,pwd,basename):
     return
 
 
-
 def main():
     # pwd=commands.getoutput('pwd')
     # basename = raw_input("Enter basename of VCF file:\n")
     pwd='/Users/josec/Desktop/Trapdoor/pyrad5/7pyrad5_outfiles/PopGenAnalysis/'
     basename="7pyrad5"
-    thin=basename+'_t'
-    pcafile=vcftoPCA(pwd,basename,thin)
-    RplotPCA(pcafile,pwd,thin)
+    pcafile=vcftoPCA(pwd,basename)
+    RplotPCA(pcafile,pwd,basename)
+    ####
+    #Comment out for Thin Options
+    ####
+    thin=thinner(pwd,basename)
+    thinpcafile=vcftoPCA(pwd,thin)
+    RplotPCA(thinpcafile,pwd,thin)
 
 if __name__ == '__main__':
     main()
