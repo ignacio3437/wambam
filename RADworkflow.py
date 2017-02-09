2b88d9db9faae899ac239531276f03f199242325
joseC <ignacio3437@gmail.com>
2017-02-09 17:43:35 +0800

    Fixing the flow each step up to R is executable indipendantly. pallete() updated again, 15 code in datafile is hotpink to designate mis       ssing Samples

--- RADworkflow.py
+++ RADworkflow.py
@@ -6,7 +6,6 @@ import scipy as sp
 from pandas import *
 from rpy2.robjects.packages import importr
 import rpy2.robjects as ro
-# from multiprocessing import Pool, TimeoutError
 grdevices = importr('grDevices')
 rprint = ro.globalenv.get("print")
 rplot=ro.r('plot')
@@ -25,21 +24,21 @@ def installtest(pwd):
     return
 
 
-def vcftoevec(pwd,basename,prune):
+def vcftoplink(pwd,basename,baseo,basep):
     os.chdir(pwd)
-    original=basename
-    vcfoutput=subprocess.check_output(shlex.split("vcftools --vcf %s.vcf --out %s_o --plink"%(basename,basename)))
-    basename=original+'_o'
+    vcfoutput=subprocess.check_output(shlex.split("vcftools --vcf %s.vcf --out %s --plink"%(basename,baseo)))
+    ##changes the chromosome all of the SNP to chromosome 1 for forward compatibility with structure like programs.
     with open("%s_temp.map"%(basename),"w") as outfile:
-        subprocess.call(shlex.split("awk '$1=1' %s.map"%(basename)),stdout=outfile)
-    print subprocess.check_output(shlex.split("mv %s_temp.map %s.map"%(basename,basename)))
-    if prune=="ON":
-        baseprune=original+'_p'
-        subprocess.check_output(shlex.split("plink2 --file %s --threads 7 --indep-pairwise 50 10 0.1"%(basename)))
-        # subprocess.check_output(shlex.split("plink2 --file %s --threads 7 --extract plink.prune.in --recode --remove W92455.txt --out %s"%(basename,baseprune)))
-        subprocess.check_output(shlex.split("plink2 --file %s --threads 7 --extract plink.prune.in --recode --geno 0.9 --mind 0.9 --out %s"%(basename,baseprune)))
+        subprocess.call(shlex.split("awk '$1=1' %s.map"%(baseo)),stdout=outfile)
+    print subprocess.check_output(shlex.split("mv %s_temp.map %s.map"%(basename,baseo)))
+    ##Prunes the dataset
+    subprocess.check_output(shlex.split("plink2 --file %s --threads 7 --indep-pairwise 50 10 0.1"%(baseo)))
+    # subprocess.check_output(shlex.split("plink2 --file %s --threads 7 --extract plink.prune.in --recode --remove W92455.txt --out %s"%(baseo,basep)))
+    subprocess.check_output(shlex.split("plink2 --file %s --threads 7 --extract plink.prune.in --recode --geno 0.9 --mind 0.9 --out %s"%(baseo,basep)))
+    return
 
-        basename=baseprune
+
+def PCAer(pwd,basep):
     with open(pwd+'pca.par','w') as pca:
         pca.write("""
     genotypename:    %s.ped
@@ -53,10 +52,9 @@ def vcftoevec(pwd,basename,prune):
     grmoutname:      grmjunk
     numthreads:   8
     lsqproject:	YES
-        """%(basename,basename,basename,basename,basename))
+        """%(basep,basep,basep,basep,basep))
     ### add     outliersigmathresh: 7 if you want less outliers
-    pcafile='%s%s.evec'%(pwd,basename)
-    pcaoutfile=pwd+basename+'_pcaout.txt'
+    pcaoutfile=pwd+basep+'_pcaout.txt'
     pcaout=subprocess.check_output(shlex.split("smartpca -p pca.par"))
     with open(pcaoutfile,'w') as pcaoutfile:
         pcaoutfile.write(pcaout)
@@ -73,12 +71,11 @@ def vcftoevec(pwd,basename,prune):
         outliers=re.findall(r'REMOVED outlier (\w*)',pcaout)
     else:
         outliers=[]
-    with open(pwd+basename+'_outliers.txt', 'w') as outlierfile:
+    with open(pwd+basep+'_outliers.txt', 'w') as outlierfile:
         for outlier in outliers:
             outlierfile.write(outlier+'\t'+outlier+'\n')
-    print "There are %d outliers in PCA analysis of %s"%(len(outliers),basename)
-    ### Create keep.txt file for pyrad with outliers removed???
-    return pcafile,axes,basename
+    print "There are %d outliers in PCA analysis of %s"%(len(outliers),basep)
+    return axes
 
 
 def RplotPCA(pcafile,pwd,basename,axes):
@@ -87,9 +84,10 @@ def RplotPCA(pcafile,pwd,basename,axes):
     ro.r('evec <- read.table(file="%s%s.evec")'%(pwd,basename))
     ro.r('dat <- read.table(file="%sdata.txt",header=TRUE)'%(pwd))
     ro.r('m2 <- merge(dat,evec,by.x="Sample",by.y="V1")')
+    ##Friendly random color palette!
     ro.r("""palette(c('#8dd3c7','#ffffb3','#bebada',
     '#fb8072','#80b1d3','#fdb462','#b3de69','#fccde5',
-    '#d9d9d9','#bc80bd','#ccebc5','#ffed6f','black','darkgrey'))""")
+    '#d9d9d9','#bc80bd','#ccebc5','#ffed6f','black','forestgreen','deeppink'))""")
     m2=ro.r['m2']
     # print m2
     ###Change color scheme by changing COI_col
@@ -189,35 +187,36 @@ def cleanup(pwd):
     finpath=pwd+"final/"
     if not os.path.exists(finpath):
         os.mkdir(finpath)
-
     for (dirpath, dirnames, filenames) in os.walk(pwd):
         for file in filenames:
             if file.endswith("png"):
                 shutil.move(pwd+file,figpath+file)
     return
 
-def controller(pwd,basename,prune,k):
-    pcafile,axes,basename=vcftoevec(pwd,basename,prune)
-    RplotPCA(pcafile,pwd,basename,axes)
-    admixture(pwd,basename,k)
-    cleanup(pwd)
+def controller(pwd,basename,k):
+    baseo='%s_o'%(basename)
+    basep='%s_p'%(basename)
+    pcafile='%s%s.evec'%(pwd,basename)
+    ##Comment out steps to skip them. PCAer() and RplotPCA() need to be run together
+    vcftoplink(pwd,basename,baseo,basep)
+    axes=PCAer(pwd,basep)
+    RplotPCA(pcafile,pwd,basep,axes)
+    # admixture(pwd,basename,k)
+    # cleanup(pwd)
     pass
 
 def main():
     # pwd=subprocess.check_output(shlex.split('pwd'))
     #basename = raw_input("Enter basename of VCF file:\n")
-    pwd='/Users/josec/Desktop/agrf3/'
+    pwd='/Users/josec/Desktop/Trapdoor/pyrad5/7pyrad5_outfiles/PopGenAnalysis/'
     basename="r_7pyrad5"
 
     installtest(pwd)
-    prune="ON"
     k=10
     bs=50
-    controller(pwd,basename,prune,k+1)
+    controller(pwd,basename,k+1)
     raxer(pwd,basename,bs)
     print "ALLDone"
-    # prune="OFF"
-    # controller(pwd,basename,prune)
     return
 
 if __name__ == '__main__':
