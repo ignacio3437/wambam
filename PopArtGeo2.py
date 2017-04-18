1834ad2c8f063b3d6c171ba9610b55c4e295d3b0
joseC <ignacio3437@gmail.com>
2017-04-18 16:52:08 +0800

    name cleaner made into a new function reads the pop_order file got rid of the controller() just going to use main()

--- PopArtGeo2.py
+++ PopArtGeo2.py
@@ -16,8 +16,20 @@ def read_param(param_txt):
             parameters.append(pwd+line.split()[0])
     return parameters
 
-def read_metadata():
-    return
+def read_popsort(pop_order):
+    with open(pop_order, "rU") as pop_order_file:
+        sorted_pops=[n.strip('\n') for n in pop_order_file.readlines()]
+    return sorted_pops
+
+def name_cleaner(line):
+    NAME_PATTERN = re.compile(r"""[A-Za-z0-9]+""")
+    OLDNAME_PATTERN = re.compile(r"""[^\[\n\t]+""")
+    namere=re.search(NAME_PATTERN, line)
+    name=namere.group()
+    oldnamere=re.search(OLDNAME_PATTERN, line)
+    oldname= oldnamere.group()
+    return [oldname,name]
+
 
 def read_popfile(pop_groups):
     pop_dict={}
@@ -27,15 +39,16 @@ def read_popfile(pop_groups):
     Sample_index=header.index('Sample')
     Pop_index=header.index('Pop')
     for line in lines[1:]:
-        pop_dict[line.split(',')[Sample_index]]=line.split(',')[Pop_index]
+        sample=line.split(',')[Sample_index]
+        [oldsample,renamed_sample]=name_cleaner(sample)
+        pop_dict[renamed_sample]=line.split(',')[Pop_index]
     print pop_dict
     return pop_dict
 
 def nexuscleanuper(nexus_file, cleanup):
     #pull out the sheets in the directory and make a list of the files and taxa for each file
+    nex_seqnames=[]
     NTAX_PATTERN = re.compile(r"""taxlabels""", re.IGNORECASE)
-    NAME_PATTERN = re.compile(r"""[A-Za-z0-9]+""")
-    OLDNAME_PATTERN = re.compile(r"""[^\[\n\t]+""")
     with open(nexus_file,'rU') as f:
         lines=f.readlines()
     with open(nexus_file,'rU') as f:
@@ -45,49 +58,29 @@ def nexuscleanuper(nexus_file, cleanup):
         if ntax:
             while ';' not in lines[i+1]:
                 nextline= lines[i+1]
-                namere=re.search(NAME_PATTERN, nextline)
-                name=namere.group()
-                oldnamere=re.search(OLDNAME_PATTERN, nextline)
-                oldname= oldnamere.group()
+                [oldname,name]=name_cleaner(nextline)
                 filestring=filestring.replace(oldname,name)
+                nex_seqnames.append(name)
                 i+=1
     outfile_name="%s_clean"%(os.path.basename(nexus_file))
     with open(os.path.join(pwd,outfile_name),"w") as outfile:
         outfile.write(filestring)
         outfile.close()
-    return
+    return nex_seqnames
 
-def seqlistmaker(nex):
-    #return the names of the sequences in nexus file
-    f=open(nex,'rU')
-    seqlist=[]
-    NTAX_PATTERN = re.compile(r"""taxlabels""", re.IGNORECASE)
-    NAME_PATTERN = re.compile(r"""[^\[\n\t]+""")
-    lines=f.readlines()
-    for i,l in enumerate(lines):
-        ntax=re.search(NTAX_PATTERN,l)
-        if ntax:
-            while ';' not in lines[i+1]:
-                nextline= lines[i+1]
-                namere=re.search(NAME_PATTERN, nextline)
-                name=namere.group()
-                seqlist.append(name)
-                i+=1
-    return seqlist
 
-def controller():
-    nexus_file="/Users/josec/Desktop/Pop2/in.nex"
-    cleanup="in_clean.nex"
-    print nexuscleanuper(nexus_file, cleanup)
-    return
 
 def main():
     parameters=read_param(os.path.join(pwd,"parameters.txt"))
-    [nexus_file,pop_groups,metadata]=parameters
-    #Check if metadata file
-    if "#" in metadata:
-        metadata=False
-    read_popfile(pop_groups)
+    [nexus_file,pop_groups,pop_order]=parameters
+    cleanup="clean_%s"%(os.path.basename(nexus_file))
+    nex_seqnames=nexuscleanuper(nexus_file, cleanup)
+    #Check if pop_order file
+    if "#" in pop_order:
+        pop_order=False
+    else:
+        sorted_pops=read_popsort(pop_order)
+    pop_dict=read_popfile(pop_groups)
     return
 
 
